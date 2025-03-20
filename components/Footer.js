99478d2b109f0ce667b963c3c4d3a2a37698ab6b
ryukichi-nana <ryukichi-nana@docomo.ne.jp>
2025-03-20 13:29:10 +0900

    ♻️ Footer部分とLinks部分をコンポーネント化

--- components/Footer.js
+++ components/Footer.js
@@ -1,26 +1,18 @@
-import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import { Footer } from "../components/Footer";
-import { Links } from "../components/Links";
 
-export default function Home() {
+export function Footer() {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Index Page</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <h1 className={styles.title}>Index Page</h1>
-
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-        <Links />
-      </main>
-
-      <Footer />
+       <footer>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Thanks by{" "}
+          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
+        </a>
+      </footer>
 
       <style jsx>{`
         main {
