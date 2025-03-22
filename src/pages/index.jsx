f4f656fa69fa7d166d346bf9f1720b165239012c
ryukichi-nana <ryukichi-nana@docomo.ne.jp>
2025-03-22 13:43:49 +0900

    ✨ useEffectを導入

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,7 @@ import styles from "src/styles/Home.module.css";
 import { Footer } from "src/components/Footer";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
 
 // const handleClick = (e) => {
 //   console.log(e.target.href);
@@ -18,7 +18,16 @@ export default function Home() {
     console.log(e.target.href);
     e.preventDefault();
     alert(foo);
-  },[]);
+  }, []);
+  useEffect(() => {
+    document.body.style.backgroundColor = "lightblue";
+    console.log("マウント時");
+
+    return () => {
+      console.log("アンマウント時");
+      document.body.style.backgroundColor = "";
+    };
+  }, []);
 
   return (
     <div className={styles.container}>
