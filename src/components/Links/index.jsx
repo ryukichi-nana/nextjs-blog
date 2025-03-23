97e1a5335af8c203a37aa61a509e006e0c51577a
ryukichi-nana <ryukichi-nana@docomo.ne.jp>
2025-03-23 17:03:24 +0900

    ♻️ リファクタリング

--- src/components/Links/index.jsx
+++ src/components/Links/index.jsx
@@ -1,10 +1,10 @@
 import classes from "src/components/Links/Links.module.css";
 
-export function Links({ items }) {
+export const Links = (props) => {
   return (
     <main>
       <div className={classes.grid}>
-        {items.map((item) => {
+        {props.items.map((item) => {
           return (
             <a key={item.href} href={item.href} className={classes.card}>
               <h3 className={classes.title}>{item.title}</h3>
