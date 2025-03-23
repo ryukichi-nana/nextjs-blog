00392e9e28dbdb6130c979c66be8f70a0d1bd1e4
ryukichi-nana <ryukichi-nana@docomo.ne.jp>
2025-03-23 14:31:27 +0900

    ♻️ 状態を変更する時にprevをつけて名前を分かりやすくした

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -13,12 +13,12 @@ export default function Home() {
   const handleClick = useCallback(() => {
     console.log(count);
     if (count < 10) {
-      setCount((count) => count + 1);
+      setCount((prevCount) => prevCount + 1);
     }
   }, [count]);
 
   const handleDisplay = useCallback(() => {
-    setIsShow((isShow) => !isShow);
+    setIsShow((prevIsShow) => !prevIsShow);
   }, []);
 
   const handleChange = useCallback((e) => {
