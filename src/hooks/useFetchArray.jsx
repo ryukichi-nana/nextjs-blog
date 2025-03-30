3551410cd9d89db818ec56a6d336bcb15773d0f9
ryukichi-nana <ryukichi-nana@docomo.ne.jp>
2025-03-30 20:03:22 +0900

    🚚 英語の間違い修正。複数形になっていたものを単数形に

--- src/hooks/useFetchArray.jsx
+++ src/hooks/useFetchArray.jsx
@@ -13,22 +13,27 @@ export const useFetchArray = (url) => {
 
 const API_URL = "https://jsonplaceholder.typicode.com";
 
-export const useComments = () => {
-  return useFetchArray(`${API_URL}/comments`);
-};
-
+// posts
 export const usePosts = () => {
   return useFetchArray(`${API_URL}/posts`);
-};
-
-export const useUsers = () => {
-  return useFetchArray(`${API_URL}/users`);
-};
-
-export const useCommentsByPostsId = (id) => {
-  return useFetchArray(id ? `${API_URL}/comments?postId=${id}`: null);
 };
 
 export const usePostsByUserId = (id) => {
   return useFetchArray(id ? `${API_URL}/posts?userId=${id}`: null);
 };
+
+// users
+export const useUsers = () => {
+  return useFetchArray(`${API_URL}/users`);
+};
+
+// comments
+export const useCommentsByPostId = (id) => {
+  return useFetchArray(id ? `${API_URL}/comments?postId=${id}`: null);
+};
+
+export const useComments = () => {
+  return useFetchArray(`${API_URL}/comments`);
+};
+
+
