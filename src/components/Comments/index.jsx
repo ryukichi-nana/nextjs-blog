58db4e03f8e5e99f0688d9cdb0fdd126694ee5f4
ryukichi-nana <ryukichi-nana@docomo.ne.jp>
2025-04-10 20:27:43 +0900

    ✨ SGとfallbackについて学びました

--- src/components/Comments/index.jsx
+++ src/components/Comments/index.jsx
@@ -21,7 +21,7 @@ export const CommentsComponent = () => {
         {data.map((comment) => {
           return (
             <li key={comment.id} className="border-b pb-2">
-              <Link href={`/comments/${comment.id}`} legacyBehavior>
+              <Link href={`/comments/${comment.id}`} prefetch={false} legacyBehavior>
                 <a className="block hover:text-blue-500">{comment.body}</a>
               </Link>
             </li>
